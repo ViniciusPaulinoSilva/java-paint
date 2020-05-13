3091f2e1af69a69a5ac9f7247c859dda4196fc63
Vinicius Paulino da Silva <49988624+ViniciusPaulinoSilva@users.noreply.github.com>
2020-05-13 16:11:57 -0300

    Elipse bugada, retangulo on

--- src/Janela.java
+++ src/Janela.java
@@ -12,6 +12,7 @@ public class Janela extends JFrame
           btnCirculo = new JButton ("Circulo"),
           btnElipse = new JButton ("Elipse"),
           btnQuadrado = new JButton("Quadrado"),
+          btnRetangulo = new JButton("Retangulo"),
           btnCorContorno = new JButton ("Contorno"),
           btnCorPreenchimento = new JButton ("Preenchimento"),
           btnAbrir = new JButton ("Abrir"),
@@ -25,7 +26,9 @@ public class Janela extends JFrame
   protected JLabel statusBar2 = new JLabel ("Coordenada:");
 
   protected boolean esperaPonto, esperaInicioReta, esperaFimReta, esperaCentro, esperaRaio,
-          esperaInicioElipse, esperaFimElipse, esperaInicioQuadrado, esperaFimQuadrado;
+          esperaInicioElipse, esperaFimElipse, esperaInicioQuadrado, esperaFimQuadrado, esperaInicioRetangulo,
+          esperaFimRetangulo;
+
   protected Color corContorno = Color.BLACK, corPreenchimento = new Color(0, 0, 0, 0);
 
   protected Ponto p1;
@@ -33,6 +36,8 @@ public class Janela extends JFrame
   protected Ponto fimElipse;
   protected Ponto inicioQuadrado;
   protected Ponto fimQuadrado;
+  protected Ponto inicioRetangulo;
+  protected Ponto fimRetangulo;
 
   protected Vector<Figura> figuras = new Vector<>();
 
@@ -109,6 +114,21 @@ public class Janela extends JFrame
       JOptionPane.showMessageDialog (
               null,
               "Arquivo quadrado.jpg n�o foi encontrado",
+              "Arquivo de imagem ausente",
+              JOptionPane.WARNING_MESSAGE
+      );
+    }
+
+    try
+    {
+      Image btnRetanguloImg = ImageIO.read(getClass().getResource("resources/retangulo.jpg"));
+      btnRetangulo.setIcon(new ImageIcon(btnRetanguloImg));
+    }
+    catch (IOException e)
+    {
+      JOptionPane.showMessageDialog (
+              null,
+              "Arquivo retangulo.jpg n�o foi encontrado",
               "Arquivo de imagem ausente",
               JOptionPane.WARNING_MESSAGE
       );
@@ -197,6 +217,7 @@ public class Janela extends JFrame
     btnCorPreenchimento.addActionListener(new EscolheCorPreenchimento());
     btnElipse.addActionListener(new DesenhoDeElipse());
     btnQuadrado.addActionListener(new DesenhoDeQuadrado());
+    btnRetangulo.addActionListener(new DesenhoDeRetangulo());
 
     JPanel pnlBotoes = new JPanel();
     FlowLayout flwBotoes = new FlowLayout();
@@ -209,6 +230,7 @@ public class Janela extends JFrame
     pnlBotoes.add(btnCirculo);
     pnlBotoes.add(btnElipse);
     pnlBotoes.add(btnQuadrado);
+    pnlBotoes.add(btnRetangulo);
     pnlBotoes.add(btnCorContorno);
     pnlBotoes.add(btnCorPreenchimento);
     pnlBotoes.add(btnApagar);
@@ -229,7 +251,7 @@ public class Janela extends JFrame
 
     this.addWindowListener(new FechamentoDeJanela());
 
-    this.setSize(1000,500);
+    this.setSize(1300,600);
     this.setVisible(true);
   }
 
@@ -340,6 +362,7 @@ public class Janela extends JFrame
         statusBar1.setText("Mensagem: ");
         esperaInicioElipse = true;
       }
+
       else if(esperaInicioQuadrado)
       {
         try
@@ -353,6 +376,7 @@ public class Janela extends JFrame
         esperaFimQuadrado = true;
         statusBar1.setText("Mensagem: Clique no ponto inferior direito do Quadrado ");
       }
+
       else if(esperaFimQuadrado)
       {
         try {
@@ -367,6 +391,37 @@ public class Janela extends JFrame
         figuras.get(figuras.size()-1).torneSeVisivel(pnlDesenho.getGraphics());
         statusBar1.setText("Mensagem: ");
         esperaInicioQuadrado = true;
+      }
+
+      else if(esperaInicioRetangulo)
+      {
+        try
+        {
+          inicioRetangulo = new Ponto (e.getX(), e.getY(), corContorno);
+        }
+        catch (Exception ex) {
+          System.out.println(ex.getMessage());
+        }
+        esperaInicioRetangulo = false;
+        esperaFimRetangulo = true;
+        statusBar1.setText("Mensagem: Clique no ponto inferior direito do retangulo ");
+      }
+
+      else if(esperaFimRetangulo)
+      {
+        try {
+          fimRetangulo = new Ponto (e.getX(), e.getY(), corContorno);
+          esperaFimRetangulo = false;
+          int largura = Math.abs(inicioRetangulo.getX() - fimRetangulo.getX());
+          int altura = Math.abs(inicioRetangulo.getY() - fimRetangulo.getY());
+          figuras.add(new Retangulo(inicioRetangulo, fimRetangulo, largura, altura, corContorno, corPreenchimento));
+        }
+        catch (Exception ex) {
+          System.out.println(ex.getMessage());
+        }
+        figuras.get(figuras.size()-1).torneSeVisivel(pnlDesenho.getGraphics());
+        statusBar1.setText("Mensagem: ");
+        esperaInicioRetangulo = true;
       }
     }
 
@@ -464,6 +519,26 @@ public class Janela extends JFrame
       esperaFimQuadrado = false;
 
       statusBar1.setText("Mensagem: clique no canto superior esquerdo do quadrado");
+    }
+  }
+
+  protected class DesenhoDeRetangulo implements ActionListener
+  {
+    public void actionPerformed (ActionEvent e)
+    {
+      esperaPonto = false;
+      esperaInicioReta = false;
+      esperaFimReta = false;
+      esperaCentro = false;
+      esperaRaio = false;
+      esperaInicioElipse = false;
+      esperaFimElipse = false;
+      esperaInicioQuadrado = false;
+      esperaFimQuadrado = false;
+      esperaInicioRetangulo = true;
+      esperaFimRetangulo = false;
+
+      statusBar1.setText("Mensagem: clique no canto superior esquerdo do retangulo");
     }
   }
 
---
+++ src/Retangulo.java
@@ -0,0 +1,141 @@
+import java.awt.*;
+import java.util.StringTokenizer;
+
+public class Retangulo extends Figura
+{
+    protected Ponto inicio; //inicio do retangulo, extremidade superior
+    protected Ponto fim; //ponto base do retangulo, extremidade inferior
+    protected int largura;
+    protected int altura;
+
+    public Retangulo(Ponto inicio, Ponto fim, int largura, int altura) throws Exception
+    {
+        this(inicio, fim, largura, altura, Color.BLACK, new Color(0, 0, 0, 0));
+    }
+
+    public Retangulo(Ponto inicio, Ponto fim, int largura, int altura, Color corContorno) throws Exception
+    {
+        this(inicio, fim, largura, altura, Color.BLACK, new Color(0, 0, 0, 0));
+    }
+
+    public Retangulo(Ponto inicio, Ponto fim, int largura, int altura, Color corContorno, Color corPreenchimento) throws Exception
+    {
+        super(corContorno, corPreenchimento);
+
+        this.fim = fim;
+        this.inicio = inicio;
+        this.largura = largura;
+        this.altura = altura;
+        this.corContorno = corContorno;
+        this.corPreenchimento = corPreenchimento;
+    }
+
+    public Retangulo(String s) //"R:inicio.X:inicio.Y:fim.X:fim.Y:R:G:B"
+    {
+        StringTokenizer quebrador = new StringTokenizer(s, ":");
+
+        quebrador.nextToken();
+
+        int iniciox = Integer.parseInt(quebrador.nextToken());
+        int inicioy = Integer.parseInt(quebrador.nextToken());
+        int fimx = Integer.parseInt(quebrador.nextToken());
+        int fimy = Integer.parseInt(quebrador.nextToken());
+
+        Color corContorno = new Color(
+                Integer.parseInt(quebrador.nextToken()),  // R
+                Integer.parseInt(quebrador.nextToken()),  // G
+                Integer.parseInt(quebrador.nextToken())   // B
+        );
+        Color corPreenchimento = new Color(
+                Integer.parseInt(quebrador.nextToken()),  // R
+                Integer.parseInt(quebrador.nextToken()),  // G
+                Integer.parseInt(quebrador.nextToken())   // B
+        );
+
+        try
+        {
+            this.inicio = new Ponto(iniciox, inicioy);
+            this.fim = new Ponto(fimx, fimy);
+            this.corContorno = corContorno;
+            this.corPreenchimento = corPreenchimento;
+        }
+
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void setInicio(int x, int y)
+    {
+        try
+        {
+            this.inicio = new Ponto (x,y,this.getCorContorno());
+        }
+
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void setFim(int x, int y)
+    {
+        try
+        {
+            this.fim = new Ponto (x,y,this.getCorContorno());
+        }
+
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Ponto getInicio()
+    {
+        return this.inicio;
+    }
+
+    public Ponto getFim()
+    {
+        return this.fim;
+    }
+    public void torneSeVisivel(Graphics g)
+    {
+        Ponto inicio = this.inicio;
+        Ponto fim = this.fim;
+        g.setColor(this.corPreenchimento);
+        this.largura = Math.abs(inicio.getX() - fim.getX());
+        this.altura = Math.abs(inicio.getY() - fim.getY());
+        g.fillRect(inicio.getX(), inicio.getY(),largura, altura);
+        g.setColor(this.corContorno);
+        g.drawRect(inicio.getX(), inicio.getY(), largura, altura);
+
+    }
+
+    public String toString()
+    {
+        return "r:" +
+                this.getInicio().getX() +
+                ":" +
+                this.getInicio().getY() +
+                ":" +
+                this.getFim().getX() +
+                ":" +
+                this.getFim().getY() +
+                ":" +
+                this.getCorContorno().getRed() +
+                ":" +
+                this.getCorContorno().getGreen() +
+                ":" +
+                this.getCorContorno().getBlue() +
+                ":" +
+                this.getCorPreenchimento().getRed() +
+                ":" +
+                this.getCorPreenchimento().getGreen() +
+                ":" +
+                this.getCorPreenchimento().getBlue();
+    }
+}
+
